a069688223a42f7a11e320a03fe4fd090b175c37
Phil Kunz <github@phil.today>
2015-09-08 19:31:47 +0000

    add package json

---
+++ ts/index.ts
@@ -0,0 +1,9 @@
+/// <reference path="typings/tsd.d.ts" />
+var path, browserSync;
+
+path = require("path");
+browserSync = require("browser-sync");
+
+module.exports = (serveDirectory) => {
+    
+};
